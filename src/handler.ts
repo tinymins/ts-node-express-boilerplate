bba2c87ee5d29e9b0489b27016e9b442fea661c5
Emil Zhai <root@derzh.com>
2023-02-08 12:50:51 +0800

    fix: handler exception catch

--- src/handler.ts
+++ src/handler.ts
@@ -12,30 +12,38 @@ import { ExpressRouterHandler } from './types';
 import { findRequestFile, httpRootPath, sendFile } from './utils/http';
 
 const handler: ExpressRouterHandler = (req, res) => {
-  const url = `${req.protocol}://${req.hostname}${req.url}`;
-  const relativePath = new URL(url).pathname || '';
+  try {
+    const url = `${req.protocol}://${req.hostname}${req.url}`;
+    const relativePath = new URL(url).pathname || '';
 
-  // local files
-  const absolutePath = findRequestFile(httpRootPath, relativePath);
-  if (absolutePath) {
-    if (absolutePath.endsWith('.ts')) {
-      try {
-        const proc = require(absolutePath);
-        proc.default(req, res);
-      } catch (error) {
-        res.send(error instanceof Error ? error.message : String(error));
+    // local files
+    const absolutePath = findRequestFile(httpRootPath, relativePath);
+    if (absolutePath) {
+      if (absolutePath.endsWith('.ts')) {
+        try {
+          const proc = require(absolutePath);
+          proc.default(req, res);
+        } catch (error) {
+          try {
+            res.send(error instanceof Error ? error.message : String(error));
+            res.end();
+          } catch {
+            console.error(error);
+          }
+        }
+
+        return;
       }
-      res.end();
-
+      sendFile(res, absolutePath);
       return;
     }
-    sendFile(res, absolutePath);
-    return;
-  }
 
-  // not found
-  res.status(404);
-  res.end();
+    // not found
+    res.status(404);
+    res.end();
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 export default handler;
